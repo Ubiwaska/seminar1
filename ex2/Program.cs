fd0f5ab952c3a8b98affb38c8c7aa81cae001636
Полина Галкина <polygal@inbox.ru>
2023-02-25 20:55:30 +0300

    added ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число 1- ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2- ");
+int numberB= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3- ");
+int numberC= Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > numberB) max = numberA;
+else
+{
+    if(numberB > numberC) max = numberB;
+    else max = numberC;
+}
+Console.Write("Максимальное число - ");
+Console.WriteLine(max);
