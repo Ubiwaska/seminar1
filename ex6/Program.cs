11c33ab5ebb2c575694e15d11837a43f0d6fa115
Полина Галкина <polygal@inbox.ru>
2023-02-25 21:31:00 +0300

    added ex6

---
+++ ex6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число- ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine("Это четное число");
+
+else Console.WriteLine("Это нечетное число");
+
