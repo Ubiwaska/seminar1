860d8b8c3cb467e5f4c4f2657e0fa07df2ba35a9
Полина Галкина <polinagalkina@MacBook-Pro.local>
2023-02-25 19:13:29 +0300

    added ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число 1- ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2- ");
+int numberB= Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > numberB) max = numberA;
+if (numberB > numberA) max = numberB;
+Console.Write("Максимальное число - ");
+Console.WriteLine(max);
