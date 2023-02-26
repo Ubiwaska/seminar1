19d1b1fde0f8add1a083ae7374aa936d33632b9f
Полина Галкина <polygal@inbox.ru>
2023-02-26 12:44:48 +0300

    added ex8

---
+++ ex8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите конечное значение диапазона 1...N");
+int n = int.Parse(Console.ReadLine());
+    if (n > 1)
+    {
+      int i = 4;
+      Console.Write(n + "-> 2");
+    while (i <= n) 
+    {
+       Console.Write("," + i);
+       i=i+2;
+    } 
+    Console.WriteLine("");
+    }
+    else
+    {
+       Console.WriteLine("Четных чисел меньше 1 не существует"); 
+    }
+     
+    
+
+
